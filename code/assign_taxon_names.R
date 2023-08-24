7ae25e09c30da69e7c31e85427c1923367313e71
Cedar Mackaness <cedarmkns@gmail.com>
2023-08-24 15:05:22 -0700

    Compare taxa present with strict denoising and relaxed denoising

--- code/assign_taxon_names.R
+++ code/assign_taxon_names.R
@@ -2,7 +2,7 @@ all_taxa_sequences <- read_csv("all_taxa_sequences.csv",
                                col_names = TRUE,
                                show_col_types = FALSE)
 
-assign_taxon_names <- function(file) {
+assign_taxon_names <- function(file, include_streams=FALSE) {
   # read in a haplo_table file
   file_data <- read_csv(file,
                    col_names = TRUE, 
@@ -10,8 +10,17 @@ assign_taxon_names <- function(file) {
   
   # join the haplo_table with the taxon information. Use the sequence columns to merge to avoid ambiguous OTU numbers.
   # select important columns.
-  complete_data <- join_left(file_data, all_taxa_sequences, by = join_by(sequences = seq)) %>% 
-    select(Phylum, Class, Order, Family, Genus, Species, haplotype)
+  if (include_streams == FALSE) {
+    complete_data <- left_join(file_data, all_taxa_sequences, by = join_by(sequences == seq)) %>% 
+      select(Phylum, Class, Order, Family, Genus, Species, haplotype, sequences) %>% 
+      filter(if_all(c(Phylum, Class, Order), ~ !is.na(.)))
+    # can choose to include site information (all the streams)
+  } else {
+    complete_data <- left_join(file_data, all_taxa_sequences, by = join_by(sequences == seq)) %>% 
+      select(-c("sort", "haplotype", "OTU.x", "OTU.y")) %>% 
+      filter(if_all(c(Phylum, Class, Order), ~ !is.na(.)))
+  }
+
   
   return(complete_data)
 }
